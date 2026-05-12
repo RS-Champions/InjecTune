127c24c098c84c0924c19ff20767e1dd0de93418
Lorenzo-StJohn <britishaccent@bk.ru>
2026-05-12 14:31:13 +0400

    chore: setup commit and branch naming rules
    * chore: configure commitlint
    
    * chore: configure validate-branch-name
    
    * refactor: move branch name vaidation into pre-push hook
    
    * docs: add commit message types into readme
    
    * style: apply prettier for readme

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test'],
+    ],
+    'header-max-length': [2, 'always', 72],
+  },
+};
