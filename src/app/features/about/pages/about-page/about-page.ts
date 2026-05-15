2d6d844bb8fa016503b84b2ce2bb4fb6b8c4f541
Lorenzo-StJohn <britishaccent@bk.ru>
2026-05-15 20:36:25 +0400

    feat: implement school-and-api section for about page

--- src/app/features/about/pages/about-page/about-page.ts
+++ src/app/features/about/pages/about-page/about-page.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AboutCard } from '../../components/about-card/about-card';
 import { AboutStore } from '../../services/about-store';
-import { TuiLoader } from '@taiga-ui/core';
+import { TuiLink, TuiLoader, TuiTitle } from '@taiga-ui/core';
+import { TuiCardLarge, TuiHeader } from '@taiga-ui/layout';
 
 @Component({
   selector: 'app-about-page',
-  imports: [AboutCard, TuiLoader],
+  imports: [AboutCard, TuiLoader, TuiCardLarge, TuiHeader, TuiLink, TuiTitle],
   templateUrl: './about-page.html',
   styleUrl: './about-page.less',
 })
