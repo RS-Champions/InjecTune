a02fa6625eef4c974cc71a22a80af6e235d74a71
Lorenzo-StJohn <britishaccent@bk.ru>
2026-05-17 17:10:35 +0400

    refactor: replace ResourceRef with HttpResourceRef

--- src/app/features/about/services/about-store.ts
+++ src/app/features/about/services/about-store.ts
@@ -1,16 +1,9 @@
-import { Injectable, inject, resource } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { httpResource, HttpResourceRef } from '@angular/common/http';
 import { TeamMember } from '../interfaces/team-member';
 
 @Injectable({ providedIn: 'root' })
 export class AboutStore {
-  private readonly http = inject(HttpClient);
   private readonly jsonUrl = 'team-members.json';
-  public readonly teamMembersResource = resource({
-    params: () => ({}),
-    loader: () => {
-      return firstValueFrom(this.http.get<TeamMember[]>(this.jsonUrl));
-    },
-  });
+  public readonly teamMembersResource: HttpResourceRef<TeamMember[] | undefined> = httpResource(() => this.jsonUrl);
 }
