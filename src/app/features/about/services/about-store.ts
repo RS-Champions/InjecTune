98dbdd0fe2756844a813e0d4ef2cbcd5f1f9eb8c
Lorenzo-StJohn <britishaccent@bk.ru>
2026-05-18 01:21:46 +0400

    refactor: update type annotation for teamMemberResource

--- src/app/features/about/services/about-store.ts
+++ src/app/features/about/services/about-store.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { httpResource, HttpResourceRef } from '@angular/common/http';
+import { httpResource } from '@angular/common/http';
 import { TeamMember } from '../interfaces/team-member';
 
 @Injectable({ providedIn: 'root' })
 export class AboutStore {
   private readonly teamMembersUrl = 'team-members.json';
-  public readonly teamMembersResource: HttpResourceRef<TeamMember[] | undefined> = httpResource(() => this.teamMembersUrl);
+  public readonly teamMembersResource = httpResource<TeamMember[]>(() => this.teamMembersUrl);
 }
