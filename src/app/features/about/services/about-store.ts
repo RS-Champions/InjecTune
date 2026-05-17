f8775e3485cbc30da5ce79a57a603f4a314467e2
Lorenzo-StJohn <britishaccent@bk.ru>
2026-05-18 01:33:10 +0400

    refactor: update folder structure and resource type
    * refactor: rename models folder into interfaces
    
    * refactor: replace ResourceRef with HttpResourceRef
    
    * refactor: rename jsonUrl into teamMembersUrl
    
    * refactor: update type annotation for teamMemberResource

--- src/app/features/about/services/about-store.ts
+++ src/app/features/about/services/about-store.ts
@@ -1,16 +1,9 @@
-import { Injectable, inject, resource } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs';
-import { TeamMember } from '../models/team-member';
+import { Injectable } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { TeamMember } from '../interfaces/team-member';
 
 @Injectable({ providedIn: 'root' })
 export class AboutStore {
-  private readonly http = inject(HttpClient);
-  private readonly jsonUrl = 'team-members.json';
-  public readonly teamMembersResource = resource({
-    params: () => ({}),
-    loader: () => {
-      return firstValueFrom(this.http.get<TeamMember[]>(this.jsonUrl));
-    },
-  });
+  private readonly teamMembersUrl = 'team-members.json';
+  public readonly teamMembersResource = httpResource<TeamMember[]>(() => this.teamMembersUrl);
 }
