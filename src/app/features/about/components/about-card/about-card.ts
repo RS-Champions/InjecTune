9c35dc09ffd937d2c60197f7be74500db5d42b46
Lorenzo-StJohn <britishaccent@bk.ru>
2026-05-20 12:25:32 +0400

    refactor: replace invisible links with pseudo-elements

--- src/app/features/about/components/about-card/about-card.ts
+++ src/app/features/about/components/about-card/about-card.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
-import { TuiAppearance, TuiLink, TuiCell } from '@taiga-ui/core';
+import { TuiAppearance, TuiLink, TuiCell, TuiIcon } from '@taiga-ui/core';
 import { TuiSkeleton } from '@taiga-ui/kit';
 import { TuiCardLarge, TuiHeader, TuiSurface } from '@taiga-ui/layout';
 import { TeamMember } from '../../models/team-member';
 
 @Component({
   selector: 'app-about-card',
-  imports: [TuiAppearance, TuiCell, TuiSkeleton, TuiCardLarge, TuiLink, TuiHeader, TuiSurface, TuiCell],
+  imports: [TuiAppearance, TuiCell, TuiSkeleton, TuiCardLarge, TuiLink, TuiHeader, TuiSurface, TuiCell, TuiIcon],
   templateUrl: './about-card.html',
   styleUrl: './about-card.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/features/about/pages/about-page/about-page.ts
+++ src/app/features/about/pages/about-page/about-page.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { TuiCardLarge } from '@taiga-ui/layout';
-import { TuiLink, TuiLoader, TuiTitle } from '@taiga-ui/core';
+import { TuiIcon, TuiLink, TuiLoader, TuiTitle } from '@taiga-ui/core';
 import { AboutCard } from '../../components/about-card/about-card';
 import { AboutStore } from '../../services/about-store';
 
 @Component({
   selector: 'app-about-page',
-  imports: [AboutCard, TuiLoader, TuiCardLarge, TuiLink, TuiTitle],
+  imports: [AboutCard, TuiLoader, TuiCardLarge, TuiLink, TuiTitle, TuiIcon],
   templateUrl: './about-page.html',
   styleUrl: './about-page.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
