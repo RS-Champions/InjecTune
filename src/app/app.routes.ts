ddb98364804d8710b1bbf82cb4631d34da40b68c
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-05-20 09:29:32 +0200

    feat: init component and implement route for it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,4 +9,9 @@ export const routes: Routes = [
     path: 'search',
     loadChildren: () => import('./features/search/search.routes').then((module) => module.SEARCH_ROUTES),
   },
+  {
+    path: 'discover',
+    loadComponent: () =>
+      import('./features/discover/pages/discover-page/discover-page').then((module) => module.DiscoverPage),
+  },
 ];
---
+++ src/app/features/discover/pages/discover-page/discover-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discover-page',
+  imports: [],
+  templateUrl: './discover-page.html',
+  styleUrl: './discover-page.less',
+})
+export class DiscoverPage {}
