16f20acbdbbd45dc28c76ff9600ffe978845231c
AlenaVP Legion <poprugo@gmail.com>
2026-05-21 23:48:29 +0200

    feat: add track mock data and update interfaaces

--- src/app/features/artist/interfaces/artist-track.ts
+++ src/app/features/artist/interfaces/artist-track.ts
@@ -1,5 +1,5 @@
 import { BaseTrack } from '@shared/track/interfaces/base-track';
 
 export interface ArtistTrack extends BaseTrack {
-  album_name: string;
+  album_name?: string;
 }
--- src/app/shared/track/interfaces/base-track.ts
+++ src/app/shared/track/interfaces/base-track.ts
@@ -3,8 +3,15 @@ export interface BaseTrack {
   name: string;
   duration: number;
   artist_name: string;
+  releasedate: string;
+  audio: string;
   stats: {
     listens_total: number;
+  };
+  musicinfo?: {
+    tags?: {
+      genres?: string[];
+    };
   };
   image?: string;
 }
