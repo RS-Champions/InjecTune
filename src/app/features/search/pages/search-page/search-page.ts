7ce70f1c4a32338de14bec23edcf199816141032
AlenaVP Legion <poprugo@gmail.com>
2026-05-22 16:15:11 +0200

    feat: update browser address bar with params while typing

--- src/app/features/search/pages/search-page/search-page.ts
+++ src/app/features/search/pages/search-page/search-page.ts
@@ -1,13 +1,14 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs';
 
 import { SearchTrack } from '@features/search/interfaces/search-track';
 import { SearchMockService } from '@features/search/services/search-mock.service';
 import { SearchTrackCard } from '@features/search/components/search-track-card/search-track-card';
 import { TuiIcon, TuiInput } from '@taiga-ui/core';
 import { TuiTooltip } from '@taiga-ui/kit';
-import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-search-page',
@@ -17,27 +18,34 @@ import { toObservable, toSignal } from '@angular/core/rxjs-interop';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchPage {
+  private readonly router = inject(Router);
   private readonly service = inject(SearchMockService);
-
-  protected readonly currentTrack = signal<SearchTrack | null>(null);
 
   protected readonly searchQuery = signal('');
   protected readonly searchResult = toSignal(
     toObservable(this.searchQuery).pipe(
       debounceTime(500),
       distinctUntilChanged(),
+      tap((query) => {
+        void this.router.navigate([], {
+          queryParams: { q: query || null },
+          queryParamsHandling: 'merge',
+        });
+      }),
       switchMap((query) => this.service.search(query)),
     ),
   );
 
+  protected readonly currentTrack = signal<SearchTrack | null>(null);
+
   onPlay(track: SearchTrack): void {
-    if (!this.currentTrack() || this.currentTrack()!.id !== track.id) {
+    if (this.currentTrack()?.id !== track.id) {
       this.currentTrack.set(track);
     }
   }
 
   onPause(track: SearchTrack): void {
-    if (this.currentTrack() && this.currentTrack()!.id === track.id) {
+    if (this.currentTrack()?.id === track.id) {
       this.currentTrack.set(null);
     }
   }
