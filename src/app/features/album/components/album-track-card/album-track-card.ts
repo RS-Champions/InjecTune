b39422c58fe0c93c231546058d72c4084acdefb9
AlenaVP Legion <poprugo@gmail.com>
2026-05-29 22:57:57 +0200

    refactor: use Equalizer component

--- src/app/features/album/components/album-track-card/album-track-card.ts
+++ src/app/features/album/components/album-track-card/album-track-card.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AlbumTrack } from '@features/album/interfaces/album-track';
+import { Equalizer } from '@shared/components/equalizer/equalizer';
 import { Track } from '@shared/track/directives/track';
 import { FormatDurationPipe } from '@shared/track/pipes/format-duration-pipe';
 import { FormatPlayCountPipe } from '@shared/track/pipes/format-play-count-pipe';
@@ -7,7 +8,7 @@ import { TuiAppearance, TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-album-track-card',
-  imports: [FormatDurationPipe, FormatPlayCountPipe, TuiButton, TuiAppearance],
+  imports: [Equalizer, FormatDurationPipe, FormatPlayCountPipe, TuiButton, TuiAppearance],
   templateUrl: './album-track-card.html',
   styleUrl: './album-track-card.less',
   host: {
--- src/app/features/artist/components/artist-track-card/artist-track-card.ts
+++ src/app/features/artist/components/artist-track-card/artist-track-card.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ArtistTrack } from '@features/artist/interfaces/artist-track';
+import { Equalizer } from '@shared/components/equalizer/equalizer';
 import { Track } from '@shared/track/directives/track';
 import { FormatDurationPipe } from '@shared/track/pipes/format-duration-pipe';
 import { FormatPlayCountPipe } from '@shared/track/pipes/format-play-count-pipe';
@@ -7,7 +8,7 @@ import { TuiAppearance, TuiButton } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-artist-track-card',
-  imports: [FormatDurationPipe, FormatPlayCountPipe, TuiButton, TuiAppearance],
+  imports: [Equalizer, FormatDurationPipe, FormatPlayCountPipe, TuiButton, TuiAppearance],
   templateUrl: './artist-track-card.html',
   styleUrl: './artist-track-card.less',
   host: {
