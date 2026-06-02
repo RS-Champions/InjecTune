2ef8d733fe9f49a904d64b1942230fc0c37af814
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-06-02 19:24:58 +0200

    feat: implement sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,25 @@
 import { Routes } from '@angular/router';
 
+export const ROUTE_PATHS = {
+  ABOUT: 'about',
+  SEARCH: 'search',
+  DISCOVER: 'discover',
+} as const;
+
 export const routes: Routes = [
   {
-    path: 'about',
+    path: ROUTE_PATHS.ABOUT,
     loadComponent: () => import('./features/about/pages/about-page/about-page').then((module) => module.AboutPage),
   },
   {
-    path: 'search',
+    path: ROUTE_PATHS.SEARCH,
     loadChildren: () => import('./features/search/search.routes').then((module) => module.SEARCH_ROUTES),
   },
   {
-    path: 'discover',
+    path: ROUTE_PATHS.DISCOVER,
     loadComponent: () =>
       import('./features/discover/pages/discover-page/discover-page').then((module) => module.DiscoverPage),
   },
 ];
+
+export type RoutePath = (typeof ROUTE_PATHS)[keyof typeof ROUTE_PATHS];
---
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -0,0 +1,38 @@
+import { Component, input, ChangeDetectionStrategy, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RoutePath } from '../../../app.routes';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
+
+export interface NavItem {
+  label: string;
+  route: RoutePath;
+  icon: string;
+}
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SidebarComponent {
+  public readonly sidebarOpen = input<boolean>(true);
+  public readonly isMobile = input<boolean>(false);
+  public readonly navigationItems = input.required<NavItem[]>();
+
+  private readonly router = inject(Router);
+  private readonly url = toSignal(
+    this.router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+      map(() => this.router.url),
+    ),
+    { initialValue: this.router.url },
+  );
+
+  protected isActiveRoute(route: string): boolean {
+    return this.url().includes(route);
+  }
+}
