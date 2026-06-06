9d34c21d635c883c7794cd576705d908a82bb604
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-06-06 23:25:42 +0200

    refactor: remove unnecessary vars and use routerLinkActive

--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,9 +1,7 @@
-import { Component, input, ChangeDetectionStrategy, inject } from '@angular/core';
-import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RoutePath } from '../../../app.routes';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { filter, map } from 'rxjs';
 
 export interface NavItem {
   label: string;
@@ -13,26 +11,11 @@ export interface NavItem {
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent {
-  public readonly sidebarOpen = input<boolean>(true);
-  public readonly isMobile = input<boolean>(false);
   public readonly navigationItems = input.required<NavItem[]>();
-
-  private readonly router = inject(Router);
-  private readonly url = toSignal(
-    this.router.events.pipe(
-      filter((event): event is NavigationEnd => event instanceof NavigationEnd),
-      map(() => this.router.url),
-    ),
-    { initialValue: this.router.url },
-  );
-
-  protected isActiveRoute(route: string): boolean {
-    return this.url().includes(route);
-  }
 }
