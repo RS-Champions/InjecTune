88a6635fea70935ec875a890370e1c8e28d18b3f
AlenaVP Legion <poprugo@gmail.com>
2026-06-13 15:08:00 +0200

    refactor: remove unnecessary input()

--- src/app/core/player/components/player-queue-item/player-queue-item.ts
+++ src/app/core/player/components/player-queue-item/player-queue-item.ts
@@ -12,7 +12,6 @@ import { TuiIcon } from '@taiga-ui/core';
 })
 export class PlayerQueueItem {
   readonly track = input.required<BaseTrack>();
-  readonly index = input.required<number>();
   readonly isActive = input(false);
 
   readonly selectTrack = output();
