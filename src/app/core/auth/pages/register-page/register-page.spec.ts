f0e15557eada688ced49d84f782e7a4fcbe2de92
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-06-30 19:01:58 +0200

    test: provide router

--- src/app/core/auth/pages/register-page/register-page.spec.ts
+++ src/app/core/auth/pages/register-page/register-page.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterPage } from './register-page';
+import { provideRouter } from '@angular/router';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
@@ -9,6 +10,7 @@ describe('RegisterPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RegisterPage],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterPage);
