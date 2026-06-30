f97fdfdde17c19c2b43ea510d6ccb8951379e115
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-06-30 17:56:45 +0200

    feat: implement mock auth service

---
+++ src/app/core/auth/services/mock-auth-service/mock-auth-service.ts
@@ -0,0 +1,75 @@
+import { computed, Injectable, signal } from '@angular/core';
+import { User } from '@core/auth/interfaces/user';
+import { delay, Observable, of, tap, throwError } from 'rxjs';
+import { AuthCredentials } from '@core/auth/interfaces/auth-credentials';
+import { AuthServiceInterface } from '../auth-service-interface/auth-service-interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MockAuthService implements AuthServiceInterface {
+  public readonly currentUser = computed(() => this._currentUser());
+
+  private readonly USERS_DB_KEY = 'auth:users_db';
+  private readonly CURRENT_USER_KEY = 'auth:current_user';
+  private _currentUser = signal<User | null>(this.getStoredUser());
+
+  public register(credentials: AuthCredentials): Observable<User> {
+    const users = this.getUsersFromDb();
+
+    if (users.some((user) => user.email === credentials.email)) {
+      return throwError(() => new Error('User with this email exist')).pipe(delay(800));
+    }
+
+    const newUser: User = {
+      id: crypto.randomUUID(),
+      email: credentials.email,
+      token: `mock-jwt-token-${crypto.randomUUID()}`,
+    };
+
+    users.push({ ...newUser, password: credentials.password });
+    localStorage.setItem(this.USERS_DB_KEY, JSON.stringify(users));
+
+    return of(newUser).pipe(
+      delay(800),
+      tap((user) => { this.setCurrentUser(user); }),
+    );
+  }
+
+  public login(credentials: AuthCredentials): Observable<User> {
+    const users = this.getUsersFromDb();
+    const user = users.find((u) => u.email === credentials.email && u.password === credentials.password);
+
+    if (!user) {
+      return throwError(() => new Error('Неверный email или пароль')).pipe(delay(800));
+    }
+
+    const userResponse: User = { id: user.id, email: user.email, token: user.token };
+
+    return of(userResponse).pipe(
+      delay(800),
+      tap((u) => { this.setCurrentUser(u); }),
+    );
+  }
+
+  public logout(): Observable<void> {
+    localStorage.removeItem(this.CURRENT_USER_KEY);
+    this._currentUser.set(null);
+    return of().pipe(delay(300));
+  }
+
+  private getUsersFromDb(): User[] {
+    return JSON.parse(localStorage.getItem(this.USERS_DB_KEY) ?? '[]') as User[];
+  }
+
+  private getStoredUser(): User | null {
+    const user = localStorage.getItem(this.CURRENT_USER_KEY);
+    if (!user) return null;
+    return JSON.parse(user) as User;
+  }
+
+  private setCurrentUser(user: User): void {
+    localStorage.setItem(this.CURRENT_USER_KEY, JSON.stringify(user));
+    this._currentUser.set(user);
+  }
+}
