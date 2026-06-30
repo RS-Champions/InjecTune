b536a3c212e83f3f91faebddefac56c4f7ee0f88
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-06-30 18:37:29 +0200

    feat: connect login page with auth service

--- src/app/core/auth/pages/login-page/login-page.ts
+++ src/app/core/auth/pages/login-page/login-page.ts
@@ -1,17 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthCredentials } from '@core/auth/interfaces/auth-credentials';
+import { AuthServiceInterface } from '@core/auth/services/auth-service-interface/auth-service-interface';
 import Input, { InputData } from '@shared/components/input/input';
-import { TuiButton } from '@taiga-ui/core';
+import { TuiButton, TuiErrorComponent } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-login-page',
-  imports: [RouterLink, Input, ReactiveFormsModule, TuiButton],
+  imports: [RouterLink, Input, ReactiveFormsModule, TuiButton, TuiErrorComponent],
   templateUrl: './login-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './login-page.less',
 })
 export class LoginPage {
+  private authService = inject(AuthServiceInterface);
+  private router = inject(Router);
+
+  protected isLoading = signal(false);
+  protected error = signal<string | null>(null);
   protected loginForm = new FormGroup({
     email: new FormControl('', [(control) => Validators.required(control), (control) => Validators.email(control)]),
     password: new FormControl('', [(control) => Validators.required(control)]),
@@ -39,6 +46,25 @@ export class LoginPage {
 
     const data = this.loginForm.getRawValue();
 
-    console.log(data);
+    this.isLoading.set(true);
+
+    const credentials: AuthCredentials = {
+      email: data.email ?? '',
+      password: data.password ?? '',
+    };
+    this.authService.login(credentials).subscribe({
+      next: () => {
+        this.isLoading.set(false);
+        void this.router.navigate(['/discover']);
+      },
+      error: (error: unknown) => {
+        this.isLoading.set(false);
+        if (error instanceof Error) {
+          this.error.set(error.message);
+        } else {
+          this.error.set('An unexpected error occurred.');
+        }
+      },
+    });
   }
 }
