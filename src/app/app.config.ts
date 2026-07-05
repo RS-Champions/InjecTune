028d0b82fd9d18df7038f80a45c819a568f53ae1
AlenaVP Legion <poprugo@gmail.com>
2026-07-05 11:56:00 +0200

    feat: add authInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withRouterConfig, withHashLocation } from '@angular/router';
+import { authInterceptor } from '@core/auth/interceptors/auth-interceptor';
 import { AuthServiceAbstract } from '@core/auth/services/auth-service-interface/auth-service-interface';
 import { BackendAuthService } from '@core/auth/services/backend-auth-service';
 import { provideTaiga } from '@taiga-ui/core';
@@ -8,6 +9,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
     { provide: AuthServiceAbstract, useClass: BackendAuthService },
     provideBrowserGlobalErrorListeners(),
     provideRouter(
@@ -21,7 +23,6 @@ export const appConfig: ApplicationConfig = {
     provideTaiga({
       mode: 'dark',
     }),
-    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true, runCoalescing: true }),
   ],
 };
