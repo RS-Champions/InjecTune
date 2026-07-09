b6bfd67f8cc9e0a076581196d4d7a4472b9c3b34
AlenaVP Legion <poprugo@gmail.com>
2026-07-10 00:29:28 +0200

    refactor: use consistent view of tui-icon which replaces undefined image

--- src/app/shared/components/music-card/music-card.ts
+++ src/app/shared/components/music-card/music-card.ts
@@ -2,11 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Equalizer } from '@shared/components/equalizer/equalizer';
 import { Track } from '@shared/track/directives/track';
 import { MusicTrack } from '@shared/track/interfaces/music-track';
-import { TuiButton } from '@taiga-ui/core';
+import { TuiButton, TuiIcon } from '@taiga-ui/core';
 
 @Component({
   selector: 'app-music-card',
-  imports: [Equalizer, TuiButton],
+  imports: [Equalizer, TuiButton, TuiIcon],
   templateUrl: './music-card.html',
   styleUrl: './music-card.less',
   host: {
