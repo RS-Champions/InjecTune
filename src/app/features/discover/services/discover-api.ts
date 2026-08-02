b47e994833ba03e377075a23ae9b08499e3c5741
Daniil <mikhalenka.d@gmail.com>
2026-08-02 14:26:28 +0200

    chore: switch mocks to real api (#183)
    * chore: switch mocks to real api
    
    * refactor: correct releaseTracksResource to releaseTracksUrl
    
    * refactor: move order into params

--- src/app/features/discover/services/discover-api.ts
+++ src/app/features/discover/services/discover-api.ts
@@ -1,15 +1,26 @@
 import { httpResource } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { BaseTrack } from '@shared/track/interfaces/base-track';
 import { JamendoResponse } from '@shared/interfaces/jamendo-response';
+import { JamendoApiBase } from '@core/jamendo/jamendo-api-base';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DiscoverApi {
-  private readonly popularTracksUrl = 'popular-tracks.json';
-  private readonly releaseTracksUrl = 'release-tracks.json';
+  private readonly jamendoApi = inject(JamendoApiBase);
 
-  public readonly popularTracksResource = httpResource<JamendoResponse<BaseTrack>>(() => this.popularTracksUrl);
-  public readonly releaseTracksResource = httpResource<JamendoResponse<BaseTrack>>(() => this.releaseTracksUrl);
+  public readonly popularTracksResource = httpResource<JamendoResponse<BaseTrack>>(() => ({
+    url: this.jamendoApi.tracksUrl,
+    params: this.discoverParams('popularity_total'),
+  }));
+
+  public readonly releaseTracksResource = httpResource<JamendoResponse<BaseTrack>>(() => ({
+    url: this.jamendoApi.tracksUrl,
+    params: this.discoverParams('releasedate_desc'),
+  }));
+
+  public discoverParams(order: string) {
+    return { ...this.jamendoApi.baseParams(), limit: '10', imagesize: 600, order };
+  }
 }
